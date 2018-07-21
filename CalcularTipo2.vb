a2fd74988c7e24f99464d27c3af4a5a793e1849c
Fernando Maccio <fernando.maccio@anima.edu.uy>
2018-07-21 16:00:28 -0300

    correccion ejercicio 2

--- CalcularTipo2.vb
+++ CalcularTipo2.vb
@@ -5,17 +5,18 @@
             MsgBox("Error al ingresar A")
         ElseIf txt2B.Text = "" Or Not Int32.TryParse(txt2B.Text, b) Then
             MsgBox("Error al ingresar B")
-        ElseIf txt2B.Text = "" Or Not Int32.TryParse(txt2B.Text, c) Then
+        ElseIf txt2B.Text = "" Or Not Int32.TryParse(txt2C.Text, c) Then
             MsgBox("Error al ingresar C")
         ElseIf txt2X.Text = "" Or Not Int32.TryParse(txt2X.Text, x) Then
             MsgBox("Error al ingresar X")
         End If
         Dim resultado1 As Integer = (a * x) + b
         Dim resultado2 As Integer = (x * x) - c
+        Dim resultado As Double = resultado1 / resultado2
         If (x * x) - c = 0 Then
             MsgBox("El resultado no existe")
         Else
-            MsgBox(String.Concat("Resultado: " + (resultado1 / resultado2)))
+            MsgBox(String.Concat("Resultado: " + resultado))
         End If
     End Sub
 End Class
